4f7822464da5f5f2a206aafeaaa6bd5f562b5e43
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-26 17:41:47 +0700

    membuat navbar dan memberikan shadow pada navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>a</div>;
+  return <Navbar />;
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          Peumoeda Atjeh
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Project
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
