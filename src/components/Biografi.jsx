11aab5ad5bb5b06cbb7332399b15e11e76b143ed
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-27 17:23:31 +0700

    penambahan svg pada section Biografi

--- src/components/Biografi.jsx
+++ src/components/Biografi.jsx
@@ -4,7 +4,7 @@ const Biografi = () => {
   return (
     <section id="Biografi">
       <div className="container">
-        <div className="row text-center my-3">
+        <div className="row text-center my-5">
           <div className="col">
             <h2>About Us</h2>
           </div>
@@ -21,6 +21,13 @@ const Biografi = () => {
           </div>
         </div>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f3f4f5"
+          fill-opacity="1"
+          d="M0,128L34.3,122.7C68.6,117,137,107,206,133.3C274.3,160,343,224,411,250.7C480,277,549,267,617,234.7C685.7,203,754,149,823,122.7C891.4,96,960,96,1029,128C1097.1,160,1166,224,1234,245.3C1302.9,267,1371,245,1406,234.7L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+        ></path>
+      </svg>
     </section>
   );
 };
