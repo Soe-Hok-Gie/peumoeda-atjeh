400766f3b45a912a62719f20ba13663c2e2fbfbd
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-29 01:32:08 +0700

    membuat 1 row dan 1 col baru utk membuat form pada halaman contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,29 @@ const Contact = () => {
             <h2>contact us</h2>
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-8">
+            <form>
+              <div class="mb-3">
+                <label for="name" class="form-label">
+                  Nama
+                </label>
+                <input type="text" class="form-control" id="name" aria-describedby="name" />
+              </div>
+              <div class="mb-3">
+                <label for="email" class="form-label">
+                  {" "}
+                  Email
+                </label>
+                <input type="email" class="form-control" id="email" aria-describedby="email" />\{" "}
+              </div>
+
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
