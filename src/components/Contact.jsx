e83248282de184e68f60d5c5d876a05124042bdc
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-29 01:39:50 +0700

    memberikan mb-3 (margin-bottom berukuran 3 pada row pertama)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section id="contact">
       <div className="container">
-        <div className="row text-center">
+        <div className="row text-center mb-3">
           <div className="col">
             <h2>contact us</h2>
           </div>
