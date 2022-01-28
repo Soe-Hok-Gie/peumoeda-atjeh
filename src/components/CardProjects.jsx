5399fdeeffedb8e0b92eeaaf4f9df850b2cee5a2
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-01-29 01:07:38 +0700

    perubahan

--- src/components/CardProjects.jsx
+++ src/components/CardProjects.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
+// nested
+// const CardProjects = (item, detail) => {
+//   return (
+//     <div class="col-md-4 mb-3">
+//       <div class="card">
+//         <ul>{detail}</ul>
+//         <img src={item.img} class="card-img-top" alt="..." />
+//         <div class="card-body">
+//           <p class="card-text">{item.desc}</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 const CardProjects = (item) => {
   return (
     <div class="col-md-4 mb-3">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import CardProjects from "./CardProjects";
 
+/// const Dataproject = [
+//   {
+//     jenis: "e-commerce",
+//     details: [{ id: 1 }, { img: "assets/1.png" }, { desc: "Some quick example text to build on the card title and make up the bulk of the card's content." }],
+//   },
+//   {
+//     jenis: "e-commerce",
+//     details: [{ id: 1 }, { img: "assets/1.png" }, { desc: "Some quick example text to build on the card title and make up the bulk of the card's content." }],
+//   },
+//   {
+//     jenis: "e-commerce",
+//     details: [{ id: 1 }, { img: "assets/1.png" }, { desc: "Some quick example text to build on the card title and make up the bulk of the card's content." }],
+//   },
+// ];
+
 const Dataproject = [
   {
     id: 1,
@@ -38,4 +53,25 @@ const Projects = () => {
   );
 };
 
+// nested map
+
+// const Projects = () => {
+//   return (
+//     <section id="project">
+//       <div className="container">
+//         <div className="row text-center mb-3">
+//           <div className="col">
+//             <h2>Project</h2>
+//           </div>
+//         </div>
+//         <div class="row mb-4">
+//           {Dataproject.map((items) => {
+//             <ul>{details.map(detail)}</ul>;
+//             return <CardProjects img={items.img} desc={items.desc} />;
+//           })}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 export default Projects;
