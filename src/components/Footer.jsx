4bc1f04984e43545da7927df1a27eb9ab11f513b
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-02-15 13:51:27 +0700

    perbaikan visible pada footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer d-none d-sm-block">
       <p>
         create by <a href="htpps://instagram/peumodaAtjeh">Peumoda Atjeh</a>
       </p>
